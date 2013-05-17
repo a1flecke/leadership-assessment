95d395d8134eed82e88fa1fec276ad26d2ff9ea7
Aaron Fleckenstein <aaron.fleckenstein@daveramsey.com>
2013-05-17 13:02:30 -0500

    Moved proejct

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-LeadershipAssessment::Application.config.secret_token = 'e648ac7f0b9823a889235969b4c4fb25e633716e94d160f685c97b39ee221c7ae8adf989fbc513f7ab629d8c885788923b369201756e410adee430d9a696d29e'
+LeadershipAssessment::Application.config.secret_token = 'edd2c111df530cebf5ae25d1a3dec3f70665cf39ff9073acae9eeaac406852b3f6e7cf4c75d31b82a267b11b4c607f0e3d350b6df668123a60beab363468e14d'
