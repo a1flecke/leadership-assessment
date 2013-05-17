4635b1493a4b12a6a1f4968280632ac00719e355
Aaron Fleckenstein <aaron.fleckenstein@daveramsey.com>
2013-05-17 12:59:08 -0500

    Initial project

---
+++ leadership-assessment/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LeadershipAssessment::Application.config.secret_token = 'e648ac7f0b9823a889235969b4c4fb25e633716e94d160f685c97b39ee221c7ae8adf989fbc513f7ab629d8c885788923b369201756e410adee430d9a696d29e'
